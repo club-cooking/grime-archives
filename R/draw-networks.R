dbef427b133328c91a60a8d01322a3a1ee56e08d
ewen_ <ewenhenderson@gmail.com>
2020-05-02 18:10:16 +0100

    fully tidy data!

--- R/draw-networks.R
+++ R/draw-networks.R
@@ -2,11 +2,14 @@
 # setup -------------------------------------------------------------------
 
 library(jsonlite)
+library(dplyr)
+library(tidyr)
+library(purrr)
+library(stringr)
 library(ggraph)
 library(tidygraph)
-library(tidyr)
-library(dplyr)
-library(purrr)
+
+source("R/functions.R")
 
 # load data ---------------------------------------------------------------
 
@@ -16,24 +19,34 @@ artists <- read_json("data/grime-artists.json", simplifyVector = TRUE)
 
 # wrangle -----------------------------------------------------------------
 
+# unnest nested objects
 tidy_releases <- unnest_releases(releases)
 tidy_tracks <- unnest_tracks(tracks)
+tidy_artists <- unnest_artists(artists)
 
-# artists -----------------------------------------------------------------
+# merge data sources
+tracks_merged <- merge_data_sources(tidy_tracks, tidy_releases, tidy_artists)
 
-# distinct grime artists
-grime_artists <- distinct(grime_releases_tidy, name, id) %>% 
-  dplyr::filter(!name %in% c("Various", "various", "Unknown Artist")) # rm Various
-
-# get metadata for grime artists
-grime_artists_info <- get_discogs_artist_meta(unique(grime_artists$id))
-
-# merge -------------------------------------------------------------------
-
-# try and join releases/tracks
-foo <- left_join(grime_releases_tidy, grime_tracks_tidy, 
-                 by = "release_id")
+# create graph df
+relations <- tracks_merged %>% 
+  mutate(style == str_c(style)) %>%
+  group_by(artist_id) %>% 
+  dplyr::filter(any(style == "Grime"), min(year) < 2005) %>% 
+  ungroup() %>% 
+  rename(from=artist_name, to=extra_artist_name) %>% 
+  as_tbl_graph() %>% 
+  mutate(n_records = centrality_degree(mode = 'in'),
+         n_have = centrality_degree(weights = community_have),
+         name = str_trim(str_remove_all(name, pattern = "\\([^\\]]*\\)")))
 
 # viz ---------------------------------------------------------------------
 
+ggraph(relations, layout = "fr") +
+  geom_edge_link(alpha = 0.25) +
+  geom_node_point(aes(size = n_have))
+  # geom_node_text(
+  #   aes(label = name, size = n_have), repel = TRUE,
+  #   color = "white", bg.color = "black", bg.r = 0.2,
+  #   family = "IBM Plex Sans Light"
+  # )
 
--- R/functions.R
+++ R/functions.R
@@ -145,6 +145,7 @@ tidy_style_releases <- function(x, style_masters, style_details) {
     select(release_id=id, everything(), -barcode, -starts_with("user_data"),
            -community.have.x, -community.have.y, -community.want.x,
            -community.want.y) %>%
+    rename(artist_name=name) %>% 
     # tidy column headers
     clean_names()
 }
@@ -170,7 +171,9 @@ unnest_releases <- function(x) {
   
   releases %>% 
     select(-artists) %>% 
-    bind_cols(artists)
+    bind_cols(artists) %>% 
+    rename(release_title=title, release_title_short=title_short,
+           artist_id=id, artist_name=name)
 }
 
 # unnest tracks data
@@ -199,10 +202,70 @@ unnest_tracks <- function(x) {
     mutate(extra_artist_name = unlist(features$name),
            extra_artist_id = unlist(features$id),
            extra_artist_role = unlist(features$role)) %>% 
-    select(-extraartists, -artists)
+    select(-extraartists, -artists) %>% 
+    rename(track_title=title) %>% 
+    group_by(release_id, track_title) %>% 
+    dplyr::filter(
+      str_detect(
+        extra_artist_role, 
+        regex("feat|prod", ignore_case = TRUE))
+      )
 }
 
 # unnest artists data
-# unnest_artists <- function(x) {
-#   
-#   }
+unnest_artists <- function(x) {
+
+  x$aliases <- map(x$aliases, function(x) {
+    
+    if (!is.null(x)) transpose(x)
+  })
+    
+  unnest_longer(x, aliases) %>% 
+    hoist(aliases,
+          alias_id = "id",
+          alias_name = "name") %>% 
+    select(
+      artist_name = name,
+      artist_id = id,
+      alias_id, alias_name
+      ) %>% 
+    unnest(c(artist_name, artist_id))
+}
+
+# merge data sources
+merge_data_sources <- function(tracks, releases, artists) {
+  
+  tracks <- tidy_tracks
+  releases <- tidy_releases
+  artists <- tidy_artists
+  
+  # join tracks/releases
+  tracks_merged <- left_join(tracks, releases, by = "release_id")
+  
+  # identify if extra artists are aliases of main artist
+  alias_matches <- map_lgl(1:nrow(tracks_merged), function(x) {
+    
+    main_artist <- tracks_merged$artist_id[x]
+    extra_artist <- tracks_merged$extra_artist_id[x]
+    
+    main_artist_aliases <- artists %>% 
+      dplyr::filter(artist_id == main_artist) %>% 
+      pull(alias_id)
+    
+    !extra_artist %in% c(main_artist, main_artist_aliases)
+  })
+  
+  # remove aliases of main artist
+  tracks_merged <- tracks_merged[alias_matches ,]
+  
+  tracks_merged %>% 
+    distinct(release_id, artist_id, extra_artist_id, track_title,
+             .keep_all = TRUE) %>% 
+    dplyr::filter(!is.na(extra_artist_id)) %>% 
+    select(
+      release_id, release_title_short, track_title, 
+      artist_id, artist_name, extra_artist_name:extra_artist_role, year,
+      style, community_have, community_want
+    )
+}
+
